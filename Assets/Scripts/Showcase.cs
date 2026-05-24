b228df414d28b66ae25cb756023c74c139cc8a2c
TheNuggetXYZ <TheNuggetXYZ@gmail.com>
2026-05-24 14:54:39 +0200

    feat: add zoom to showcase

--- Assets/Scripts/Showcase.cs
+++ Assets/Scripts/Showcase.cs
@@ -3,14 +3,19 @@ using UnityEngine;
 
 public class Showcase : MonoBehaviour
 {
-    [SerializeField] private Transform rotationTransform;
-    
-    private Transform RotationTransform => rotationTransform != null ? rotationTransform : transform;
+    [SerializeField] private Transform cameraPivot;
+    [SerializeField] private new Transform camera;
+    [SerializeField] private float scrollSpeed = 50;
+    [SerializeField] private float rotationSpeed = 1;
 
     private void Update()
     {
         Vector2 lookInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         
-        RotationTransform.Rotate(0, -lookInput.x, -lookInput.y);
+        cameraPivot.Rotate(0, -lookInput.x * rotationSpeed, -lookInput.y * rotationSpeed);
+        
+        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
+        
+        camera.Translate(Vector3.forward * scrollInput * scrollSpeed, Space.Self);
     }
 }
